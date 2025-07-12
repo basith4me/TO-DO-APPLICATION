2b1ff73c83f8d57aabd1e03a0806b03b44455e0f
Abdul Basith <116253542+basith4me@users.noreply.github.com>
2025-07-13 00:42:09 +0530

    feat: initialize client with React, Vite, and Tailwind CSS
    - Added package.json for client with dependencies and scripts
    - Created main application structure with App component
    - Integrated Tailwind CSS for styling
    - Set up Vite configuration for development and build
    - Added SVG assets for React and Vite logos
    
    feat: implement server with Todo API
    
    - Created Mongoose model for Todo items
    - Developed RESTful API routes for CRUD operations on Todo items
    - Integrated Express server with routes and middleware for JSON parsing
    - Connected to MongoDB database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { connectDB } = require('./configs/db.js');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const { connectDB } = require("./configs/db.js");
+const todoRoutes = require("./routes/todo.route.js")
 const app = express();
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(5000, () => {
-    connectDB();
-    console.log('Server is running on port 5000');
-})
+  connectDB();
+  console.log("Server is running on port 5000");
+});
